eeacf6ac13ae7a57e6c15e372101c6c01565164c
unknown <kitharveycaubalejo@gmail.com>
2023-05-11 19:03:56 +0800

    set up vue

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<style scoped>
+.card {
+  background-color: var(--white);
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
+    by <a href="https://www.frontendmentor.io/profile/kitharvey" target="_blank">kit harvey</a>.
+  </footer>
+</template>
+
+<style scoped>
+.attribution {
+  font-size: 11px;
+  text-align: center;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
